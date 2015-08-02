cb73c7f42698bc2331115a6727d7038df11990e7
James Croft <jmcroft@outlook.com>
2015-08-02 15:45:55 +0100

    Aded Menu Item template and modified the main page for the app

---
+++ AppTemplate/AppTemplate/Controls/Models/SplitViewPaneMenuItem.cs
@@ -0,0 +1,19 @@
+﻿namespace AppTemplate.Controls.Models
+{
+    using System;
+
+    using Windows.UI.Xaml.Controls;
+
+    public class SplitViewPaneMenuItem
+    {
+        public string Label { get; set; }
+
+        public Symbol Symbol { get; set; }
+
+        public char SymbolAsChar => (char)this.Symbol;
+
+        public Type AssociatedPage { get; set; }
+
+        public object NavigationArguments { get; set; }
+    }
+}
